2114cb5ca28dd00adca0bc4bcecfd243cbe8e089
IsadoraAbreuu <isaabreucorrea@gmail.com>
2025-08-20 16:44:58 -0300

    Aula do Lindomar

---
+++ api/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255)
+    data_nascimento = models.DateField(null=True, blank=True)
+    nacionalidade = models.CharField(max_length=30, null=True, blank=True)
+    biografia = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
+
+
