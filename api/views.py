290201574879dd95f4cc4951b2b65c425da21b83
IsadoraAbreuu <isaabreucorrea@gmail.com>
2025-08-21 17:00:15 -0300

    Atualizações

--- api/views.py
+++ api/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
-from .models import Autor
-from .serializers import AutorSerializers
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Autor, Editora, Livro
+from .serializers import AutorSerializers, EditoraSerializers, LivroSerializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(['GET', 'POST'])
+def visualizacao_autor(request):
+    if request.method == 'GET':
+        queryset = Autor.objects.all()
+        serializer = AutorSerializers(queryset, many = True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = AutorSerializers(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+#----------------- AUTORES ----------------
 class AutoresView(ListCreateAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializers
+
+class AutoresDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializers
+
+#----------------- EDITORAS -----------------
+class EditorasView(ListCreateAPIView):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializers
+
+class EditorasDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializers 
+
+#---------------- LIVROS --------------------
+class LivrosView(ListCreateAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializers
+
+class LivrosDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializers
