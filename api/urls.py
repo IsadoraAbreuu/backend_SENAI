8951613bbe822e3a7a1be0e27cc5d1c1509fb09a
IsadoraAbreuu <isaabreucorrea@gmail.com>
2025-08-28 16:54:18 -0300

    jwt and filters

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
 
@@ -8,9 +13,14 @@ urlpatterns = [
     path('authors', visualizacao_autor),
     path('editoras', EditorasView.as_view()),
     path('livros', LivrosView.as_view()),
+    path('buscar/', AutoresView.as_view()),
 
     # UPDATE / DELETE
     path('autor/<int:pk>', AutoresDetailView.as_view()),
     path('editora/<int:pk>', EditorasDetailView.as_view()),
     path('livro/<int:pk>', LivrosDetailView.as_view()),
+
+    # TOKEN
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
